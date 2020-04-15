78173ca8b52df0528086c755e7434db814bb0df6
angelique-m08 <mora.angelique@gmail.com>
2020-04-15 19:08:29 +0200

    refactor(app-textinput): create api call and show results

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,37 @@
 import React from 'react';
 import SearchBar from './components/SearchBar';
 import './App.css';
-import CocktailList from './components/CocktailList';
+/*import CocktailList from './components/CocktailList';*/
+import Axios from 'axios';
 
-function App() {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      keywords: null,
+      cocktails: []
+    };
+  }
+
+setKeywords = (keywords) => this.setState({keywords})
+
+search = () => {
+  Axios.get(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${this.state.keywords}`)
+    .then((response) => response.data)
+    .then((data) => {
+      this.setState({ cocktails: data.drinks.map((drink) => drink.strDrink)
+      });
+    });
+}
+
+render() {
   return (
     <div className="App">
-      <SearchBar />
-      <CocktailList />
+      <SearchBar setKeywords={this.setKeywords} onSearch={this.search} />
+  {this.state.cocktails.map((cocktail) => <li>{cocktail}</li>)}
     </div>
   );
+}
 }
 
 export default App;
