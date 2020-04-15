3dd161115f2b746dfa8095d45d2c0a3b56f317c9
angelique-m08 <mora.angelique@gmail.com>
2020-04-15 19:04:24 +0200

    refactor(button): implementation of search function

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
+const Button = ({onClick}) => (
+  <button onClick={onClick} type="button">
+    Search
+  </button>
+);
 
-
-export default Button
+export default Button;
