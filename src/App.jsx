4013b53025ffb9457cec745b3895c879ab283b8b
jojo051 <youpi051@yahoo.fr>
2020-04-22 11:54:57 +0200

    implentation axios

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Axios from 'axios';
 import SearchBar from './components/SearchBar';
 import './components/makeYourCocktail.css';
-import CocktailList from './components/CocktailList'
+import CocktailList from './components/CocktailList';
 
 
 class App extends React.Component {
@@ -11,10 +11,30 @@ class App extends React.Component {
     this.state = {
       keywords: null,
       cocktails: [],
+      allCocktails: [],
     };
   }
 
 setKeywords = (keywords) => this.setState({ keywords })
+
+getData = () => {
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+  for (let i = 0; i < 26; i++){
+  
+    setTimeout( () => {
+
+      (Axios.get(`https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${alphabet[i]}`,)
+      .then((response) => response.data)
+      .then((data) => {this.setState({allCocktails: this.state.allCocktails.push(data.drinks)
+        })
+      })
+    )
+    },500)
+  }
+console.log(this.state.allCocktails);
+}
+
+
 
 search = () => {
   Axios.get(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${this.state.keywords}`)
@@ -28,7 +48,7 @@ render() {
   return (
     <div className="makeYourCocktail">
       <div className="searchBar">
-        <SearchBar setKeywords={this.setKeywords} onSearch={this.search} />
+        <SearchBar setKeywords={this.setKeywords} onSearch={this.getData} />
       </div>
       <div>
         <CocktailList list = {this.state.cocktails} />
