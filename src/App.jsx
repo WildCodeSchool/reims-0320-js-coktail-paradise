15a41a0b96e850b54e04e39d8ff970a4efd9a8db
jojo051 <youpi051@yahoo.fr>
2020-04-22 12:09:59 +0200

    refactor(app)boucle for

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
-import React from 'react';
-import Axios from 'axios';
-import SearchBar from './components/SearchBar';
-import './components/makeYourCocktail.css';
-import CocktailList from './components/CocktailList';
-
+import React from "react";
+import Axios from "axios";
+import SearchBar from "./components/SearchBar";
+import "./components/makeYourCocktail.css";
+import CocktailList from "./components/CocktailList";
 
 class App extends React.Component {
   constructor(props) {
@@ -15,49 +14,48 @@ class App extends React.Component {
     };
   }
 
-setKeywords = (keywords) => this.setState({ keywords })
+  setKeywords = (keywords) => this.setState({ keywords });
 
-getData = () => {
-  const alphabet = 'abcdefghijklmnopqrstuvwxyz';
-  for (let i = 0; i < 26; i++){
-  
-    setTimeout( () => {
+  getData = () => {
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+    for (let i = 0; i < 26; i++) {
+      setTimeout(() => {
+        Axios.get(
+          `https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${alphabet[i]}`,
+        )
+          .then((response) => response.data)
+          .then((data) => {
+            this.setState({
+              allCocktails: this.state.allCocktails.push(data.drinks),
+            });
+          });
+      }, 500);
+    }
+    console.log(this.state.allCocktails);
+  };
 
-      (Axios.get(`https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${alphabet[i]}`,)
-      .then((response) => response.data)
-      .then((data) => {this.setState({allCocktails: this.state.allCocktails.push(data.drinks)
-        })
-      })
+  search = () => {
+    Axios.get(
+      `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${this.state.keywords}`
     )
-    },500)
+      .then((response) => response.data)
+      .then((data) => {
+        this.setState({ cocktails: data.drinks });
+      });
+  };
+
+  render() {
+    return (
+      <div className="makeYourCocktail">
+        <div className="searchBar">
+          <SearchBar setKeywords={this.setKeywords} onSearch={this.getData} />
+        </div>
+        <div>
+          <CocktailList list={this.state.cocktails} />
+        </div>
+      </div>
+    );
   }
-console.log(this.state.allCocktails);
-}
-
-
-
-search = () => {
-  Axios.get(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${this.state.keywords}`)
-    .then((response) => response.data)
-    .then((data) => {
-      this.setState({ cocktails: data.drinks });
-    });
-}
-
-render() {
-  return (
-    <div className="makeYourCocktail">
-      <div className="searchBar">
-        <SearchBar setKeywords={this.setKeywords} onSearch={this.getData} />
-      </div>
-      <div>
-        <CocktailList list = {this.state.cocktails} />
-      </div>
-      
-
-    </div>
-  );
-}
 }
 
 export default App;
