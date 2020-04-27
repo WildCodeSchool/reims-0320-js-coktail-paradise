a618b36f75580bbdf172284e833a146512392215
jojo051 <youpi051@yahoo.fr>
2020-04-27 18:52:25 +0200

    feat(coctail):add recipe cocktail

--- src/components/Cocktail.jsx
+++ src/components/Cocktail.jsx
@@ -1,15 +1,29 @@
 import React from 'react';
-import './cocltail.css'
-
-
+import './makeYourCocktail.css';
+import Axios from 'axios';
 class Cocktail extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { clicked: false };
+    this.state = {
+      clicked: false,
+      cocktailDescription: '',
+    };
+  }
+
+  componentDidMount() {
+    this.description();
   }
 
   clicked = () => {
     this.setState({ clicked: !this.state.clicked });
+  }
+
+  description = () => {
+    Axios.get(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${this.props.cocktail.idDrink}`)
+      .then((response) => response.data)
+      .then((data) => {
+        this.setState({ cocktailDescription: data.drinks[0].strInstructions });
+      });
   }
 
   render() {
@@ -20,9 +34,9 @@ class Cocktail extends React.Component {
         </button>
         <div className={this.state.clicked? 'showDescriptionOn' : 'hideDescription'}>
           <div>
-            
+            <h1>  {this.state.cocktailDescription} </h1>
+            <img className="cocktailThumb" src={this.props.cocktail.strDrinkThumb} alt={this.props.cocktail.strDrink} />
           </div>
-          <img className="cocktailThumb" src={this.props.cocktail.strDrinkThumb} alt = {this.props.cocktail.strDrink}/>
         </div>
       </div>
     );
