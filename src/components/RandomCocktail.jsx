8a835dba83f3377037b68aeeab1f7a9212e0ad31
jojo051 <youpi051@yahoo.fr>
2020-05-05 16:59:31 +0200

    refector(RandomCocktail):ajout ingredient

--- src/components/RandomCocktail.jsx
+++ src/components/RandomCocktail.jsx
@@ -5,7 +5,8 @@ class RandomCocktail extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-    randomCocktail: [],
+    randomCocktail: "",
+    
     };
   }
 
@@ -13,15 +14,35 @@ class RandomCocktail extends React.Component {
     Axios.get('https://www.thecocktaildb.com/api/json/v1/1/random.php')
       .then((response) => response.data)
       .then((data) => {
-        this.setState({ randomCocktail: data.drinks[0].strDrink });
+        this.setState({ randomCocktailName: data.drinks[0]});
       });
   }
 
   render() {
     return (
-      <div className="randomCoctail">
-        <h1> Your Random Cocktail </h1>
-        <this.state.randomCocktail/>
+      <div>
+        <div className="randomCoctail">
+          <h1> Your Random Cocktail </h1>
+          <div>
+            <img src={this.state.randomCoktail.strDrinkThumb} 
+              alt={this.state.randomCocktail.strDrink} />
+          </div>
+          <h2>{this.state.randomCocktail.strDrink}</h2>
+          <div>
+            <h3>Ingredient:</h3>
+            <ul>
+              <li> {this.state.randomCocktail.strIngredient1} </li>
+              <li> {this.state.randomCocktail.strIngredient2} </li>
+              <li> {this.state.randomCocktail.strIngredient3} </li>
+              <li> {this.state.randomCocktail.strIngredient4} </li>
+              <li> {this.state.randomCocktail.strIngredient5} </li>
+            </ul>
+          </div>
+          <div>
+            <h3> Instructions </h3>
+            <p> {this.state.randomCocktail.strInstructions} </p>
+          </div>
+        </div>
       </div>
     );
   }
