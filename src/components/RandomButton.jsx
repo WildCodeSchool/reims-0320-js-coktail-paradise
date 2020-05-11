03bed0ef00a0e7dcf8387d7182700c16831f9171
jojo051 <youpi051@yahoo.fr>
2020-05-11 09:50:39 +0200

    feat(random):add component buttonRandom and randomCocktail

---
+++ src/components/RandomButton.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const RandomButton = ({ onClick }) => (
+  <button onClick={onClick} type="button">
+    View cocktail random
+  </button>
+);
+export default RandomButton;
---
+++ src/components/RandomCocktail.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import Axios from 'axios';
+
+class RandomCocktail extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      randomCocktail: [],
+    };
+  }
+
+  componentDidMount() {
+    this.searchRandomCocktail();
+  }
+
+  searchRandomCocktail = () => {
+    Axios.get('https://www.thecocktaildb.com/api/json/v1/1/random.php')
+      .then((response) => response.data)
+      .then((data) => {
+        this.setState({ randomCocktailName: data.drinks[0] });
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="randomCoctail">
+          <h1> Your Random Cocktail </h1>
+          <div>
+            <img src={this.state.randomCoktail.strDrinkThumb}
+              alt={this.state.randomCocktail.strDrink}
+            />
+          </div>
+          <h2>{this.state.randomCocktail.strDrink}</h2>
+          <div>
+            <h3>Ingredient:</h3>
+            <ul>
+              <li> {this.state.randomCocktail.strIngredient1} </li>
+              <li> {this.state.randomCocktail.strIngredient2} </li>
+              <li> {this.state.randomCocktail.strIngredient3} </li>
+              <li> {this.state.randomCocktail.strIngredient4} </li>
+              <li> {this.state.randomCocktail.strIngredient5} </li>
+            </ul>
+          </div>
+          <div>
+            <h3> Instructions </h3>
+            <p> {this.state.randomCocktail.strInstructions} </p>
+          </div>
+          <button type="button" onClick={this.searchRandomCocktail}>new random cocktail</button>
+        </div>
+      </div>
+    );
+  }
+}
+export default RandomCocktail;
