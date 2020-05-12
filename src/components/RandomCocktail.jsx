3cdab84247c164379f43936751798a19ee2b3ff2
jojo051 <youpi051@yahoo.fr>
2020-05-12 14:25:08 +0200

    feat(random):prepare the css

--- src/components/RandomCocktail.jsx
+++ src/components/RandomCocktail.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Axios from 'axios';
+import './randomCocktail.css'
 class RandomCocktail extends React.Component {
   constructor(props) {
     super(props);
@@ -10,6 +11,7 @@ class RandomCocktail extends React.Component {
 
   componentDidMount() {
     this.searchRandomCocktail();
+    {console.log(this.state.randomCocktail)}
   }
 
   searchRandomCocktail = () => {
@@ -19,33 +21,43 @@ class RandomCocktail extends React.Component {
         this.setState({ randomCocktail: data.drinks[0] });
       });
   }
-  
+
   render() {
     return (
-      <div>
-        <div className="randomCoctail">
-          <h1> Your Random Cocktail </h1>
-          <div>
-            <img src={this.state.randomCocktail.strDrinkThumb}
-              alt={this.state.randomCocktail.strDrink}
-            />
-          </div>
+      <div className="randomCoctail">
+        <img className="" src={this.state.randomCocktail.strDrinkThumb}
+          alt={this.state.randomCocktail.strDrink}
+        />
+        <div className="randomCoctail-title">
+          <h1> Your Random Cocktail: </h1>
           <h2>{this.state.randomCocktail.strDrink}</h2>
-          <div>
-            <h3>Ingredient:</h3>
-            <ul>
-              <li>{this.state.randomCocktail.strIngredient1}</li>
-              <li>{this.state.randomCocktail.strIngredient2}</li>
-              <li>{this.state.randomCocktail.strIngredient3}</li>
-              <li>{this.state.randomCocktail.strIngredient4}</li>
-              <li>{this.state.randomCocktail.strIngredient5}</li>
-              <li>{this.state.randomCocktail.forEach((ingredients) => ingredients.include(() => ) )}</li>
-            </ul>
-          </div>
-          <div>
-            <h3> Instructions </h3>
-            <p>{this.state.randomCocktail.strInstructions}</p>
-          </div>
+        </div>
+        <div className="randomCoctail-ingredients">
+          <h3>Ingredient:</h3>
+          <ul>
+            <li>{this.state.randomCocktail.strIngredient1}</li>
+            <li>{this.state.randomCocktail.strIngredient2}</li>
+            <li>{this.state.randomCocktail.strIngredient3}</li>
+            <li>{this.state.randomCocktail.strIngredient4}</li>
+            <li>{this.state.randomCocktail.strIngredient5}</li>
+            <li>{this.state.randomCocktail.strIngredient6}</li>
+            <li>{this.state.randomCocktail.strIngredient7}</li>
+            <li>{this.state.randomCocktail.strIngredient8}</li>
+            <li>{this.state.randomCocktail.strIngredient9}</li>
+            <li>{this.state.randomCocktail.strIngredient10}</li>
+            <li>{this.state.randomCocktail.strIngredient11}</li>
+            <li>{this.state.randomCocktail.strIngredient12}</li>
+            <li>{this.state.randomCocktail.strIngredient13}</li>
+            <li>{this.state.randomCocktail.strIngredient14}</li>
+            <li>{this.state.randomCocktail.strIngredient15}</li>
+          {/*this.state.randomCocktail.keys().filter((key) => key.startsWith('strIngredient')).map((stringredient) => console.log(stringredient))*/}
+          </ul>
+        </div>
+        <div className="randomCoctail-instruction">
+          <h3> Instructions </h3>
+          <p>{this.state.randomCocktail.strInstructions}</p>
+        </div>
+        <div className="randomCoctail-button">
           <button type="button" onClick={this.searchRandomCocktail}>new random cocktail</button>
         </div>
       </div>
