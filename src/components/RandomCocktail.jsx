ceabb5232d4969f3d1fb2054cf54dea014e4c87e
jojo051 <youpi051@yahoo.fr>
2020-05-12 18:27:24 +0200

    refector(randomcss)firstview

--- src/components/RandomCocktail.jsx
+++ src/components/RandomCocktail.jsx
@@ -28,33 +28,38 @@ class RandomCocktail extends React.Component {
   render() {
     return (
       <div className="randomCoctail">
+        <h1> Your Random Cocktail: </h1>
         <div>
           <Link className="homeButton" to="/">X</Link>
         </div>
-        <img className="randomCocktail-img"
-          src={this.state.randomCocktail.strDrinkThumb}
-          alt={this.state.randomCocktail.strDrink}
-        />
-        <div className="randomCoctail-title">
-          <h1> Your Random Cocktail: </h1>
-          <h2>{this.state.randomCocktail.strDrink}</h2>
-        </div>
-        <div className="randomCoctail-ingredients">
-          <h3>Ingredient:</h3>
-          <ul>
-            {Object.keys(this.state.randomCocktail).filter(
-              (key) => key.startsWith('strIngredient') && this.state.randomCocktail[key] != null,
-            ).map(
-              (strIngredient) => <li>{this.state.randomCocktail[strIngredient]}</li>,
-            )}
-          </ul>
+        <div className="randomCoctail-up">
+          <img className="randomCocktail-img"
+            src={this.state.randomCocktail.strDrinkThumb}
+            alt={this.state.randomCocktail.strDrink}
+          />
+          <div>
+            <div className="randomCoctail-title">
+              
+              <h2>{this.state.randomCocktail.strDrink}</h2>
+            </div>
+            <div className="randomCoctail-ingredients">
+              <h3>Ingredient:</h3>
+              <ul>
+                {Object.keys(this.state.randomCocktail).filter(
+                  (key) => key.startsWith('strIngredient') && this.state.randomCocktail[key] != null,
+                ).map(
+                  (strIngredient) => <li>{this.state.randomCocktail[strIngredient]}</li>,
+                )}
+              </ul>
+            </div>
+          </div>
         </div>
         <div className="randomCoctail-instruction">
           <h3> Instructions </h3>
           <p>{this.state.randomCocktail.strInstructions}</p>
         </div>
-        <div className="randomCoctail-button">
-          <button type="button" onClick={this.searchRandomCocktail}>new random cocktail</button>
+        <div>
+          <button className="randomCoctail-button" type="button" onClick={this.searchRandomCocktail}>New random cocktail</button>
         </div>
       </div>
     );
