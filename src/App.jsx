504eb56fbabe6966ba66094a6675af875a5d3b88
lucas <lucas-p52@hotmail.fr>
2020-05-12 15:17:37 +0200

    refactor(app): fix my mistake

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,32 @@ import NavBar from './components/NavBar';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import MakeYourCocktail from "./components/MakeYourCocktail";
 import Home from "./components/Home";
-import ChooseYourCocktail from  "./components/ChooseYourCocktail"
+import ChooseYourCocktail from "./components/ChooseYourCocktail"
+import PopupAge from './components/PopupAge';
+import './components/popupAge.css';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showPopup: true,
+    };
+  }
+
+  togglePopup = () => {
+    this.setState({ showPopup: !this.state.showPopup });
+  }
+
+  exitSite = () => {
+    window.history.back();
+  }
 
   render() {
     return (
       <Router>
+        <div>
+          {this.state.showPopup && <PopupAge closePopupEnter={this.togglePopup} closePopupExit={this.exitSite} /> }
+        </div>
         <Switch>
           <Route path="/cocktails/create">
             <MakeYourCocktail />
